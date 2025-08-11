cd23ec3ae4a1eff0f503c9dc8bd2397b50da806e
Lachlan Roberts <lachlan.p.roberts@gmail.com>
2025-08-11 15:53:45 +1000

    test staged jetty version of 12.1.0
    Signed-off-by: Lachlan Roberts <lachlan.p.roberts@gmail.com>

--- stagedReleaseTester.sh
+++ stagedReleaseTester.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export AVAILABLE_JETTY_VERSIONS="$*"
-export REPOSITORY_URL="http://localhost:8081/repository/maven-public/org/eclipse/jetty"
+export REPOSITORY_URL="http://10.0.0.15:8081/repository/maven-public/org/eclipse/jetty"
 ./update.sh
